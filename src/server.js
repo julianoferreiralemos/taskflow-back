aa2f0284cda734fcce4198b2f22bf5f3de98eb80
julianoferreiralemos <julianolemos1101@gmail.com>
2026-05-13 19:59:51 -0300

    feat: estrutura inicial do backend

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const tasks = [
+  { id: 1, title: "Estudar CI/CD" },
+  { id: 2, title: "Deploy no Render" }
+];
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API rodando com sucesso!"
+  });
+});
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
